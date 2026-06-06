07a628a0eec5ba9ebd715e7129cda559cc4fc875
Chidiebere <chidichekwas@yahoo.com>
2026-06-05 20:46:15 -0500

    Fix remove AWS CLI install and harden Slack step 5

--- main.tf
+++ main.tf
@@ -1,71 +1,61 @@
-resource "aws_iam_role" "lambda_start_pipeline_role" {
-  name = "lambda-start-pipeline-role"
+terraform {
+  required_version = ">= 1.0"
 
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect = "Allow"
-      Principal = { Service = "lambda.amazonaws.com" }
-      Action = "sts:AssumeRole"
-    }]
-  })
-}
-
-resource "aws_iam_role_policy" "lambda_start_pipeline_policy" {
-  role = aws_iam_role.lambda_start_pipeline_role.id
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Action = ["sagemaker:StartPipelineExecution"]
-        Resource = "arn:aws:sagemaker:${var.aws_region}:${var.account_id}:pipeline/${var.pipeline_name}"
-      },
-      {
-        Effect = "Allow"
-        Action = [
-          "logs:CreateLogGroup",
-          "logs:CreateLogStream",
-          "logs:PutLogEvents"
-        ]
-        Resource = "*"
-      }
-    ]
-  })
-}
-
-resource "aws_lambda_function" "start_pipeline" {
-  function_name = var.lambda_function_name
-  role          = aws_iam_role.lambda_start_pipeline_role.arn
-  handler       = "start_pipeline.lambda_handler"
-  runtime       = "python3.10"
-
-  filename = var.lambda_zip_path
-
-  environment {
-    variables = {
-      PIPELINE_NAME = var.pipeline_name
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
   }
 }
 
-resource "aws_lambda_permission" "allow_s3_invoke" {
-  statement_id  = "AllowS3Invoke"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.start_pipeline.function_name
-  principal     = "s3.amazonaws.com"
-  source_arn    = "arn:aws:s3:::${var.bucket_name}"
+provider "aws" {
+  region = var.aws_region
 }
 
-resource "aws_s3_bucket_notification" "fraud_processed_trigger" {
-  bucket = var.bucket_name
+# -----------------------------
+# Lambda Function
+# -----------------------------
+resource "aws_lambda_function" "start_pipeline" {
+  function_name = "${var.pipeline_name}-starter"
+  handler       = "lambda_function.lambda_handler"
+  runtime       = "python3.10"
 
-  lambda_function {
-    lambda_function_arn = aws_lambda_function.start_pipeline.arn
-    events              = ["s3:ObjectCreated:*"]
-    filter_prefix       = "fraud/processed/"
-  }
+  # Correct usage of path.module
+  filename         = "${path.module}/${var.lambda_zip_path}"
+  source_code_hash = filebase64sha256("${path.module}/${var.lambda_zip_path}")
 
-  depends_on = [aws_lambda_permission.allow_s3_invoke]
+  role = aws_iam_role.lambda_exec_role.arn
+}
+
+# -----------------------------
+# IAM Role for Lambda
+# -----------------------------
+resource "aws_iam_role" "lambda_exec_role" {
+  name = "${var.pipeline_name}-lambda-exec-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action    = "sts:AssumeRole"
+      Effect    = "Allow"
+      Principal = { Service = "lambda.amazonaws.com" }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_basic" {
+  role       = aws_iam_role.lambda_exec_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+# -----------------------------
+# Outputs
+# -----------------------------
+output "lambda_function_name" {
+  value = aws_lambda_function.start_pipeline.function_name
+}
+
+output "lambda_role_arn" {
+  value = aws_iam_role.lambda_exec_role.arn
 }
--- variables.tf
+++ variables.tf
@@ -1,32 +1,25 @@
 variable "aws_region" {
-  description = "AWS region for deployment"
   type        = string
-  default     = "us-east-1"
+  description = "AWS region to deploy resources"
 }
 
 variable "pipeline_name" {
+  type        = string
   description = "Name of the SageMaker pipeline"
-  type        = string
-}
-
-variable "account_id" {
-  description = "AWS account ID"
-  type        = string
 }
 
 variable "bucket_name" {
-  description = "S3 bucket that triggers the Lambda"
   type        = string
+  description = "S3 bucket for pipeline scripts and artifacts"
+}
+
+variable "account_id" {
+  type        = string
+  description = "AWS account ID"
 }
 
 variable "lambda_zip_path" {
-  description = "Path to the Lambda ZIP file"
   type        = string
-  default     = "${path.module}/lambda/lambda_start_pipeline.zip"
-}
-
-variable "lambda_function_name" {
-  description = "Lambda function name"
-  type        = string
-  default     = "start-fraud-pipeline-on-s3"
+  description = "Relative path to the Lambda ZIP file"
+  default     = "lambda/lambda_start_pipeline.zip"
 }
